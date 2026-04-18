9dd1384e7d896b38015afb43490a2884159f7953
Jhonma <jhonma1238@gmail.com>
2026-04-18 14:41:07 -0500

    Add initial implementation of restaurant service with constants, DTOs, and repository interfaces

---
+++ src/main/java/com/pragma/plazoletaservice/application/constants/ApplicationConstants.java
@@ -0,0 +1,19 @@
+package com.pragma.plazoletaservice.application.constants;
+
+public class ApplicationConstants {
+
+    private ApplicationConstants(){
+    }
+
+    public static final String RESTAURANT_NAME_CANNOT_BE_BLANK_MESSAGE = "Restaurant name cannot be blank";
+    public static final String RESTAURANT_NAME_ONLY_NUMBERS = "Restaurant name cannot contain only numbers";
+    public static final String NIT_CANNOT_BE_BLANK_MESSAGE = "NIT cannot be blank";
+    public static final String NIT_ONLY_NUMBERS = "NIT must contain only numbers";
+    public static final String ADDRESS_CANNOT_BE_BLANK_MESSAGE = "Address cannot be blank";
+    public static final String PHONE_NUMBER_CANNOT_BE_BLANK_MESSAGE = "Phone number cannot be blank";
+    public static final String PHONE_NUMBER_INVALID_FORMAT_MESSAGE = "Phone number must be in a valid format, e.g., +1234567890";
+    public static final String LOGO_CANNOT_BE_BLANK_MESSAGE = "Logo URL cannot be blank";
+    public static final String OWNER_ID_CANNOT_BE_NULL_MESSAGE = "Owner ID cannot be null";
+    public static final String OWNER_ID_ONLY_NUMBERS_MESSAGE = "Owner ID must contain only numbers";
+
+}
