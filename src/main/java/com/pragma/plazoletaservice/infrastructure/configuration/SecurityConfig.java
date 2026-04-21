4fdc9ab1bfd730a2de4e4e2771870b9c7ea6eddf
Jhonma <jhonma1238@gmail.com>
2026-04-21 16:58:43 -0500

    Update security configuration to allow public access to all requests

--- src/main/java/com/pragma/plazoletaservice/infrastructure/configuration/SecurityConfig.java
+++ src/main/java/com/pragma/plazoletaservice/infrastructure/configuration/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/restaurants").hasRole("ADMIN")
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 );
         http.addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
